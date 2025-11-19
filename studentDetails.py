2ef95f800ea1db913ead7d5235e97c4d561d7a10
nikhilpujar06 <nikihilpujar3@gmail.com>
2025-11-19 09:54:50 +0530

    save CIE-2 file

---
+++ studentDetails.py
@@ -0,0 +1,19 @@
+import sys 
+if (sys.argv) == 5:
+    script_name = sys.argv[0]
+    student_name = sys.argv[1]
+    student_USN = sys.argv[2]
+    student_department = sys.argv[3]
+    student_semester = sys.argv[4]
+else: 
+    script_name = sys.argv[0]
+    student_name = "Nikhil"
+    student_USN = "01fe24bca004"
+    student_department = "BCA"
+    student_semester = 3
+
+print("----- Student Details -----")
+print(f"\nStudent Name : {student_name}")
+print(f"\nStudent USN: {student_USN}")
+print(f"Student Department: {student_department}")
+print(f"Student Semester: {student_semester}")
