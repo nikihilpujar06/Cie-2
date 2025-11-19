5ed252335d811b031343893e6140c4124dcfca4c
nikhilpujar06 <nikihilpujar3@gmail.com>
2025-11-19 10:11:05 +0530

    save CIE-2 File

--- studentDetails.py
+++ studentDetails.py
@@ -5,12 +5,15 @@ if (sys.argv) == 4:
     student_USN = sys.argv[2]
     student_department = sys.argv[3]
     student_semester = sys.argv[4]
+    print(len(sys.argv))
+    print("User Input")
 else: 
     script_name = sys.argv[0]
     student_name = "Nikhil"
     student_USN = "01fe24bca004"
     student_department = "BCA"
     student_semester = 3
+    print("Default values")
 
 print("----- Student Details -----")
 print(f"\nStudent Name : {student_name}")
